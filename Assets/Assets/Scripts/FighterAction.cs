efef5304a82b7a08f9d27302121d456735fc6217
Rifai-13 <130067475+Rifai-13@users.noreply.github.com>
2023-11-30 23:44:49 +0700

    aset

---
+++ Assets/Assets/Scripts/FighterAction.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FighterAction : MonoBehaviour
+{
+    private GameObject hero;
+    private GameObject enemy;
+
+    [SerializeField]
+    private GameObject meleePrefab;
+
+    [SerializeField]
+    private GameObject rangePrefab;
+
+    [SerializeField]
+    private Sprite faceIcon;
+
+    private GameObject currentAttack;
+    
+    void Awake()
+    {
+        hero = GameObject.FindGameObjectWithTag("Hero");
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+    }
+    public void SelectAttack(string btn)
+    {
+        GameObject victim = hero;
+        try
+        {
+            if (tag == "Hero")
+        {
+            victim = enemy;
+        }
+        if (btn.CompareTo("BesickAttack") == 0)
+        {
+            Debug.Log("BesickAttack");
+           // meleePrefab.GetComponent<AttackScript>().Attack(victim);
+
+        } else if (btn.CompareTo("Health") == 0)
+        {
+            Debug.Log("Health");
+           // rangePrefab.GetComponent<AttackScript>().Attack(victim);
+        } else
+        {
+            Debug.Log("AttackUp");
+        }
+        }
+        catch (System.NullReferenceException e)
+        {
+            
+            Debug.Log("terjadi kesalahan" + e.Message);
+        }
+        
+    }
+}
---
+++ Assets/Assets/Scripts/MakeButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeButton : MonoBehaviour
+{
+    [SerializeField]
+    private bool physical;
+
+    private GameObject hero;
+    void Start()
+    {
+        string temp = gameObject.name;
+        gameObject.GetComponent<Button>().onClick.AddListener(() => AttachCallback(temp));
+        hero = GameObject.FindGameObjectWithTag("Hero");
+    }
+
+    private void AttachCallback(string btn)
+    {
+        try
+        {
+            if (btn.CompareTo("BesickAttackBtn") == 0)
+            {
+                hero.GetComponent<FighterAction>().SelectAttack("BesickAttack");
+            }
+            else if (btn.CompareTo("RangeBtn") == 0)
+            {
+                hero.GetComponent<FighterAction>().SelectAttack("Health");
+            }
+            else
+            {
+                hero.GetComponent<FighterAction>().SelectAttack("AttackUp");
+            }
+        }
+        catch (System.NullReferenceException e)
+        {
+            Debug.Log("terjadi kesalahan" + e.Message);
+            
+        }
+
+    }
+}
